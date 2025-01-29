a74d96741a838195adfefe018a7a273afabb81e3
SoliditySam <bitcoin4cashqc@hotmail.com>
2025-01-29 12:48:27 -0500

    wrong ccontract name

--- solidity/contracts/MockERC20.sol
+++ solidity/contracts/MockERC20.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 import "../node_modules/openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MOCKUSDC is ERC20 {
+contract MockUSDC is ERC20 {
     constructor(uint256 initialSupply) ERC20("Fake USDC", "USDC") {
         _mint(msg.sender, initialSupply);
     }
